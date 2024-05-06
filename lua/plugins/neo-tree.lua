672272ad14accaf1fe7ffab59df604621a6fd917
dohyun <dohyun_kim@brown.edu>
2024-05-05 22:59:27 -0400

    tmux integration

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,21 +1,40 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  branch = 'v3.x',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  config = function()
-    require('neo-tree').setup {
-      event_handlers = {
-        {
-          event = 'file_opened',
-          handler = function(file_path)
-            require('neo-tree.command').execute { action = 'close' }
-          end,
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+    config = function()
+      require('neo-tree').setup {
+        event_handlers = {
+          {
+            event = 'file_opened',
+            handler = function(file_path)
+              require('neo-tree.command').execute { action = 'close' }
+            end,
+          },
         },
-      },
-    }
-  end,
+      }
+    end,
+  },
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+    },
+    keys = {
+      { '<c-h>',  '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>',  '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>',  '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>',  '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
 }
