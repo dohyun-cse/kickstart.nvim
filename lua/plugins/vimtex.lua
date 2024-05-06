b266d82d0edf2727cbd1b2efb8365a8af6d47b4a
dohyun <dohyun_kim@brown.edu>
2024-05-05 21:00:36 -0400

    vimtex

---
+++ lua/plugins/vimtex.lua
@@ -0,0 +1,18 @@
+vim.g.maplocalleader = ' '
+return {
+  'lervarg/vimtex',
+  init = function()
+    vim.g.vimtex_view_method = 'skim'
+    vim.g.vimtex_view_skim_sync = 1
+    vim.g.vimtex_skim_activate = 0
+    vim.g.vimtex_quickfix_ignore_filters = {
+      'Underfull',
+      'Overfull',
+      'Tight',
+      'LaTeX Font Warning',
+      'Package breakurl Warning',
+    }
+    vim.g.vimtex_syntax_enabled = 0
+    vim.g.vimtex_syntax_conceal_disable = 1
+  end,
+}
