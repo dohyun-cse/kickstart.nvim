2051995aee4835c5cd89726bb4597fba66a6e2ad
dohyun <dohyun_kim@brown.edu>
2024-05-05 21:04:54 -0400

    autoclose neotree when file opened

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,5 +7,15 @@ return {
     'MunifTanjim/nui.nvim',
   },
   config = function()
+    require('neo-tree').setup {
+      event_handlers = {
+        {
+          event = 'file_opened',
+          handler = function(file_path)
+            require('neo-tree.command').execute { action = 'close' }
+          end,
+        },
+      },
+    }
   end,
 }
