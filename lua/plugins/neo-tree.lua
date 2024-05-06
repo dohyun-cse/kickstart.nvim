9a7838162a45797b9d73d50f0599d0fb1f7488e5
dohyun <dohyun_kim@brown.edu>
2024-05-05 23:57:03 -0400

    hide diagnostic

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -12,6 +12,7 @@ return {
         event_handlers = {
           {
             event = 'file_opened',
+---@diagnostic disable-next-line: unused-local
             handler = function(file_path)
               require('neo-tree.command').execute { action = 'close' }
             end,
