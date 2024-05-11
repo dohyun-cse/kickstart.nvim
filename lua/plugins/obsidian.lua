f4e54aa9056a46f71142ad84ae09a9c97b13c4f1
dohyun <dohyun_kim@brown.edu>
2024-05-11 15:54:01 -0400

    format and keymap -wip

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,22 +1,32 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",
+  'epwalsh/obsidian.nvim',
+  version = '*',
   lazy = true,
-  ft = "markdown",
+  ft = 'markdown',
   dependencies = {
     -- Required.
-    "nvim-lua/plenary.nvim",
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp',
+    'nvim-telescope/telescope.nvim',
   },
   opts = {
     workspaces = {
       {
-        name = "personal",
-        path = "~/vaults/personal",
+        name = 'Personal',
+        path = '~/vaults/Personal',
       },
       {
-        name = "work",
-        path = "~/vaults/work",
+        name = 'Work',
+        path = '~/vaults/Work',
       },
     },
   },
+  config = function()
+    require('obsidian').setup {
+      picker = { name = 'telescope.nvim', mapping = { insert_link = '<leader>l' } },
+      mappings = {
+        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      },
+    }
+  end,
 }
