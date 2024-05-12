2f5fa366593add2a978789f5718cdc3463dff1be
dohyun <dohyun_kim@brown.edu>
2024-05-12 14:10:37 -0400

    Harpoon save on toggle

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,9 +1,9 @@
 return {
-  'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    local harpoon = require 'harpoon'
-    harpoon:setup()
+    local harpoon = require("harpoon")
+    harpoon:setup({ settings = { save_on_toggle = true } })
   end,
 }
