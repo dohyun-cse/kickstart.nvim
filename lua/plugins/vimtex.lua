f02b649ec578f059f57074f1099ead0a06b18b6c
dohyun <dohyun_kim@brown.edu>
2024-06-19 13:21:21 -0400

    format

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,17 +1,17 @@
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = " "
 return {
-  'lervag/vimtex',
-  init = function()
-    vim.g.vimtex_view_method = 'skim'
-    vim.g.vimtex_view_skim_sync = 1
-    vim.g.vimtex_skim_activate = 0
-    vim.g.vimtex_quickfix_ignore_filters = {
-      'Underfull',
-      'Overfull',
-      'Tight',
-      'LaTeX Font Warning',
-      'Package breakurl Warning',
-      'Package caption Warning: Unknown document class (or package), standard defaults will be used.',
-    }
-  end,
+	"lervag/vimtex",
+	init = function()
+		vim.g.vimtex_view_method = "skim"
+		vim.g.vimtex_view_skim_sync = 1
+		vim.g.vimtex_skim_activate = 0
+		vim.g.vimtex_quickfix_ignore_filters = {
+			"Underfull",
+			"Overfull",
+			"Tight",
+			"LaTeX Font Warning",
+			"Package breakurl Warning",
+			"Package caption Warning: Unknown document class (or package), standard defaults will be used.",
+		}
+	end,
 }
