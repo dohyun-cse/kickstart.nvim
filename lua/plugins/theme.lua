fb7d203eec30a47502e09a7ce044af5f2a676d39
dohyun <dohyun_kim@brown.edu>
2024-06-25 13:37:11 -0400

    dashboard - error

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,22 @@
 return {
-  -- 'Mofiqul/dracula.nvim',
-  'catppuccin/nvim',
-  lazy = false,
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'catppuccin-mocha'
-  end,
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+			require("dashboard").setup({
+				-- config
+			})
+		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	},
+	{
+		-- 'Mofiqul/dracula.nvim',
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 }
